86423516ec7fce4af2058bd9ba9d2d6f4a00c9a1
Only <ongranly@ngs.ru>
2023-03-01 12:32:31 +0700

    ДЗ Unit тестирование

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -19,6 +19,13 @@ class UserTest {
     }
 
     @Test
+    public void shouldCreateUserWhenParametersIsNotExists() {
+        out = new User();
+        assertEquals(DEFAULT_LOGIN, out.getLogin());
+        assertEquals(DEFAULT_EMAIL, out.getEmail());
+    }
+
+    @Test
     public void emailValidation() {
         out = new User(CORRECT_LOGIN, CORRECT_EMAIL);
         assertTrue(out.getEmail().matches(EMAIL_REGEX));
@@ -30,12 +37,5 @@ class UserTest {
         assertNotEquals(out.getEmail(), out.getLogin());
         out = new User(CORRECT_LOGIN, CORRECT_EMAIL);
         assertNotEquals(out.getEmail(), out.getLogin());
-    }
-
-    @Test
-    public void shouldCreateUserWhenParametersIsNotExists() {
-        out = new User();
-        assertEquals(DEFAULT_LOGIN, out.getLogin());
-        assertEquals(DEFAULT_EMAIL, out.getEmail());
     }
 }
