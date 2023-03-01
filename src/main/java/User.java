2edcf7045c0d35451922ab4a611cf6f5a583f312
Only <ongranly@ngs.ru>
2023-03-01 12:30:54 +0700

    ДЗ Unit тестирование

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,5 +1,3 @@
-import java.util.Objects;
-
 public class User {
     private final String login;
     private final String email;
@@ -20,18 +18,5 @@ public class User {
 
     public String getEmail() {
         return email;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return Objects.equals(login, user.login) && Objects.equals(email, user.email);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(login, email);
     }
 }
